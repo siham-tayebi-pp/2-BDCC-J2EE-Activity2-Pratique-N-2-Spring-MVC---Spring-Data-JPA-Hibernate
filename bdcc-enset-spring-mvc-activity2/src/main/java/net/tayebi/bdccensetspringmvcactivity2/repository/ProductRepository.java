93c7282101fc102c9d8fb173cca9560940b75361
SIHAM TAYEBI <siham.tayebi-etu@etu.univh2c.ma>
2026-04-13 00:33:17 +0100

    Delete bdcc-enset-spring-mvc-activity2 directory

--- bdcc-enset-spring-mvc-activity2/src/main/java/net/tayebi/bdccensetspringmvcactivity2/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package net.tayebi.bdccensetspringmvcactivity2.repository;
-
-import net.tayebi.bdccensetspringmvcactivity2.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
